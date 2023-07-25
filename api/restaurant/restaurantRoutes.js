fab4d5c533d2af8c5327be44861b743417b0665a
ganganth <2020cs058@stu.ucsc.cmb.ac.lk>
2023-07-25 12:07:10 +0530

    get user details

--- api/restaurant/restaurantRoutes.js
+++ api/restaurant/restaurantRoutes.js
@@ -4,6 +4,13 @@ const router = express.Router();
 const app = express();
 const orders = require('../../models/ordersSchema');
 
+
+
+
+
+
+
+
 router.get("/res_details_get",async (req, res) => {
     const user_id = req.body.user_id;
     
