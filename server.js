49f3a6a51f20df9d39f0e4ff69c9eeed282d9db7
madhawaucsc2453 <2020cs157@stu.ucsc.cmb.ac.lk>
2023-08-06 19:16:03 +0530

    git commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 var cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+
+
 const userRoutes = require('./api/UserRoutes');
 const restaurantRoutes = require('./api/restaurant/restaurantRoutes');
 const path = require('path');
@@ -15,6 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api', userRoutes);
 app.use('/api', restaurantRoutes);
 app.use('/uploads', express.static('./uploads'));
+
 
 app.listen(5001, () => {
 	console.log('Server listening on port 5001');
